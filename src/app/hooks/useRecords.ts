989323cd59320e40c43ee30f662e583e3b5b5ffc
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2023-06-30 17:16:56 +0900

    feat: add fetch record Hook

---
+++ src/app/hooks/useRecords.ts
@@ -0,0 +1,28 @@
+import useSWR from 'swr'
+import { DailyCost, TotalDailyCost } from '../api/dailyCost/repositories'
+import moment from 'moment'
+import { DateFormat } from '@/utils/format'
+
+export function useFetchRecord() {
+  return useSWR('/api/dailyCost', async (url: string) => {
+    const response = await fetch(url)
+    const res = (await response.json()) as DailyCost[]
+
+    const groupData: { [key: string]: TotalDailyCost } = {}
+    res.forEach((e) => {
+      const key = moment(e.createdAt).format(DateFormat)
+      if (groupData[key]) {
+        groupData[key].records.push(e)
+        groupData[key].cost += Number(e.cost)
+      } else {
+        groupData[key] = {
+          date: key,
+          records: [e],
+          cost: 0,
+        }
+      }
+    })
+
+    return groupData
+  })
+}

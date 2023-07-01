f9959c06b1d95d57ef7431ad71756dcc48560f6d
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2023-07-01 12:36:23 +0900

    feat: adjust page component height

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import {
-  CostType,
-  DailyCost,
-  TotalDailyCost,
-} from '../api/dailyCost/repositories'
+import { TotalDailyCost } from '../api/dailyCost/repositories'
 import DailyCostCircle from '../components/graphics/daily-cost'
 import CostTrend from '../components/graphics/cost-trend'
 import CostCategoryBarChart from '../components/graphics/cost-category'
 import RecordTable from './record-table'
-import { FormatDateTimeString } from '@/utils/format'
-import useSWR from 'swr'
 import { useFetchRecord } from '../hooks/useRecords'
-import { Box, Stack, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 
 export default function Dashboard() {
   const { data, error, isLoading } = useFetchRecord()
@@ -22,18 +16,22 @@ export default function Dashboard() {
 
   return (
     <>
-      <Typography className='my-3' variant='h6'>
+      <Typography
+        className='my-2 text-black'
+        variant='h6'
+        sx={{ maxHeight: 30 }}
+      >
         Dashboard
       </Typography>
       <div className='h-full py-3'>
         <div className='h-full flex flex-row flex-wrap space-between justify-center'>
           <div className='w-full md:w-full lg:w-1/2'>
-            <DailyCostCircle className='h-1/2' todayCost={1000} />
-            <CostTrend className='h-1/2' data={chartData} />
+            <DailyCostCircle className='text-black' data={data ?? {}} />
+            <RecordTable className='mt-5' data={data ?? {}} />
           </div>
 
           <div className='w-full md:w-full lg:w-1/2 mb-2'>
-            <RecordTable className='mt-5 h-1/2' data={data ?? {}} />
+            <CostTrend className='h-1/2' data={chartData} />
             <CostCategoryBarChart className='h-1/2' data={chartData} />
           </div>
         </div>

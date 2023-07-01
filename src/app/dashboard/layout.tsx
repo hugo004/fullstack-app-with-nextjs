baf69feb59ca5b2800e82e623b6eeaed896dd5d0
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2023-07-01 12:35:50 +0900

    feat: change to right background

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import { ReactElement } from 'react'
 
 export default function DashboardLayout(props: { children: ReactElement }) {
-  return <div className='dashboard-layout p-5 h-screen'>{props.children}</div>
+  return (
+    <div className='dashboard-layout p-5 h-screen bg-white'>
+      {props.children}
+    </div>
+  )
 }

5caca9627fcd013583e0a330b9dcf7eb61b4c9d3
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2023-07-03 12:49:35 +0900

    feat: add record hook method

--- src/app/hooks/useRecords.ts
+++ src/app/hooks/useRecords.ts
@@ -6,6 +6,7 @@ import {
 } from '../api/dailyCost/repositories'
 import moment from 'moment'
 import { DateFormat } from '@/utils/format'
+import useSWRMutation from 'swr/mutation'
 
 export function useFetchRecord() {
   return useSWR('/api/dailyCost', async (url: string) => {
@@ -35,4 +36,63 @@ export function useFetchRecord() {
 
     return groupData
   })
+}
+
+export function useCreateRecord() {
+  return useSWRMutation(
+    '/api/dailyCost',
+    async (url: string, payload: { arg: Omit<DailyCost, 'id'> }) => {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload.arg),
+      })
+
+      if (!response.ok) {
+        throw new Error('An error occurred during fetch')
+      }
+
+      return response.json()
+    }
+  )
+}
+
+export function useUpdateRecord() {
+  return useSWRMutation(
+    '/api/dailyCost',
+    async (url: string, payload: { arg: DailyCost }) => {
+      const response = await fetch(`${url}/${payload.arg.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload.arg),
+      })
+
+      if (!response.ok) {
+        throw new Error('An error occurred during update')
+      }
+
+      return response.json()
+    }
+  )
+}
+
+export function useDeleteRecord() {
+  return useSWRMutation(
+    '/api/dailyCost',
+    async (url: string, payload: { arg: { id: string } }) => {
+      const response = await fetch(`${url}/${payload.arg.id}`, {
+        method: 'DELETE',
+      })
+
+      if (!response.ok) {
+        throw new Error('An error occurred during delete')
+      }
+
+      return response.json()
+    }
+  )
 }

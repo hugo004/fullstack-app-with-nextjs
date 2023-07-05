827aa39a44605f9bc70e2243babe01e181b8f704
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2023-07-05 17:40:17 +0900

    feat: add console log for server side api

--- src/app/api/dailyCost/route.ts
+++ src/app/api/dailyCost/route.ts
@@ -4,35 +4,41 @@ import { DailyCost } from './repositories'
 import moment from 'moment'
 
 export async function GET(request: NextRequest) {
+  console.log('[GET] /api/dailyCost')
   // queryable params
   const name = request.nextUrl.searchParams.get('name')
   const cost = request.nextUrl.searchParams.get('cost')
   const startDate = request.nextUrl.searchParams.get('startDate')
   const endDate = request.nextUrl.searchParams.get('endDate')
-
-  const result = await prisma.dailyCost.findMany({
-    where: {
-      name: {
-        contains: name || undefined,
+  try {
+    const result = await prisma.dailyCost.findMany({
+      where: {
+        name: {
+          contains: name || undefined,
+        },
+        cost: cost ? Number(cost) : undefined,
+        date:
+          startDate || endDate
+            ? {
+                gte: startDate
+                  ? moment(startDate).startOf('day').toISOString()
+                  : undefined,
+                lte: endDate
+                  ? moment(endDate).endOf('day').toISOString()
+                  : undefined,
+              }
+            : undefined,
       },
-      cost: cost ? Number(cost) : undefined,
-      date:
-        startDate || endDate
-          ? {
-              gte: startDate
-                ? moment(startDate).startOf('day').toISOString()
-                : undefined,
-              lte: endDate
-                ? moment(endDate).endOf('day').toISOString()
-                : undefined,
-            }
-          : undefined,
-    },
-    orderBy: {
-      date: 'desc',
-    },
-  })
-  return NextResponse.json(result, { status: 200 })
+      orderBy: {
+        date: 'desc',
+      },
+    })
+
+    return NextResponse.json(result, { status: 200 })
+  } catch (err) {
+    console.error('[GET] /dailyCost', err)
+    return NextResponse.error()
+  }
 }
 
 export async function POST(request: NextRequest) {
